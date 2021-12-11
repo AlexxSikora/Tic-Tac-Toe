7d8a9feab83cafc0990f8d3a3e93ae7ae6cbbf28
AlexxSikora <alex.sikora@onet.pl>
2021-12-11 15:08:27 +0100

    interaction v2

--- script.js
+++ script.js
@@ -4,25 +4,28 @@ let oBtn = document.querySelector('.playerO');
 let gameBoard = document.querySelector('.game-board');
 let boxes = [...document.querySelectorAll('.box')];
 
-let clickedXBtn = (xBtn.onclick = () => {
+let playerX = 'fas fa-times';
+let playerO = 'far fa-circles';
+
+clickedXBtn = () => {
 	xBtn.classList.add('selected');
 	oBtn.classList.add('hide');
-});
+};
 
-let clickedOBtn = (oBtn.onclick = () => {
+clickedOBtn = () => {
 	oBtn.classList.add('selected');
 	xBtn.classList.add('hide');
-});
+};
 
 xBtn.addEventListener('click', clickedXBtn);
 oBtn.addEventListener('click', clickedOBtn);
 
-clickedBox = (element) => {
-	if (clickedXBtn.classList.contains('selected')) {
-		element.innerHTML = '<i class="fas fa-times"></i>';
-	} else {
-		element.innerHTML = '<i class="far fa-times"></i>';
-	}
-};
-
-boxes.forEach((box) => box.addEventListener('Click', clickedBox));
+boxes.forEach((box) =>
+	box.addEventListener('click', function clickedBox(element) {
+		if (xBtn.classList.contains('selected')) {
+			element.innerHTML = `<i class= "${playerX}"></i>`;
+		} else {
+			element.innerHTML = `<i class= "${playerO}"></i>`;
+		}
+	})
+);
